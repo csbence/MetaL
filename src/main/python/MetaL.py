f3ef0cda9b72010b3c9c52d6e4d42b20fceb6009
Bence Cserna <bence@cserna.net>
2017-09-20 15:02:41 -0400

    Initial project

---
+++ src/main/python/MetaL.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LogisticRegression
+from sklearn import datasets
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
