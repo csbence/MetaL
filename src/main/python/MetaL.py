31f974f3e2f5220ed8d49f44c215238ad96b1685
jramsdell <ramsdell.jordan@gmail.com>
2017-09-20 15:34:42 -0400

    Add dataframe reading from csv.

--- src/main/python/MetaL.py
+++ src/main/python/MetaL.py
@@ -1,12 +1,32 @@
 import numpy as np
+import pandas
 import matplotlib.pyplot as plt
 
 from sklearn.linear_model import LogisticRegression
 from sklearn import datasets
 
 
-def main():
+def read_dataframe(filename):
+    col_names = ["Elevation", "Aspect", "Slope",
+                 "Horizontal_Distance_To_Hydrology",
+                 "Vertical_Distance_To_Hydrology",
+                 "Horizontal_Distance_To_Roadways",
+                 "Hillshade_9am", "Hillshade_Noon", "Hillshade_3pm",
+                 "Horizontal_Distance_To_Fire_Points"]
+    col_names.extend("WA_{}".format(i) for i in range(4))
+    col_names.extend("ST_{}".format(i) for i in range(40))
+    col_names.append("Cover_Type")
+    dataframe = pandas.read_csv(filename, names=col_names)
+    return dataframe
+
+
+def do_logistic_regression(dataframe):
     pass
+
+
+def main():
+    dataframe = read_dataframe("../../../resources/covtype/covtype.data.gz")
+    do_logistic_regression(dataframe)
 
 
 if __name__ == "__main__":
