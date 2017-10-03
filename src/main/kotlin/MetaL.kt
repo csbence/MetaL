e86fcaaeda827a508f46aa6142a4402346e794fa
Bence Cserna <bence@cserna.net>
2017-10-03 11:07:33 -0400

    Fix initial version

--- src/main/kotlin/MetaL.kt
+++ src/main/kotlin/MetaL.kt
@@ -1,4 +1,7 @@
 
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.spark.ml.classification.DecisionTreeClassifier
 import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.ml.classification.OneVsRest
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
@@ -8,58 +11,128 @@ import org.apache.spark.sql.Row
 import org.apache.spark.sql.SparkSession
 
 fun main(args: Array<String>) {
-    println("> MetaL <")
+    println("_________________________")
+    println(">>>>>>>>> MetaL <<<<<<<<<")
+    println("The Meta Learning Library")
+    println("_________________________")
+
+    Logger.getLogger("org").level = Level.OFF
+    Logger.getLogger("akka").level = Level.OFF
 
     val spark = SparkSession.builder()
             .master("local[*]")
             .appName("Example")
             .orCreate
 
-    val covtypeData: Dataset<Row> = spark.sqlContext()
-            .read()
-            .option("inferSchema", true)
-            .csv("/Users/bencecserna/Documents/Development/projects/MetaL/resources/covtype/covtype.data.gz")
-            .limit(1000)
-            .toDF()
-            .withColumnRenamed("_c54", "label")
-            .cache()
+    spark.sparkContext().setLogLevel("ERROR")
 
-    covtypeData.printSchema()
+    val data = loadCovtype(spark)
 
-    val labelledData = VectorAssembler()
-            .setInputCols(Array(4, {"_c${it + 10}"}))
-            .setOutputCol("features")
-            .transform(covtypeData)
-            .select("label", "features")
-            .cache()
-
-    val randomSplit = labelledData.randomSplit(doubleArrayOf(0.4, 0.6), 1L)
-    val trainingSet = randomSplit[0].cache()
-    val testSet = randomSplit[1].cache()
-
-
-    println(trainingSet.first())
-    trainingSet.show(3, false)
-
-    val logisticRegression = LogisticRegression()
-            .setLabelCol("label")
-            .setFeaturesCol("features")
-            .setMaxIter(10)
-            .setFitIntercept(true)
-
-    val classifier = OneVsRest()
-            .setClassifier(logisticRegression)
-
-    val oneVsRestModel = classifier.fit(trainingSet)
-    val predictions = oneVsRestModel.transform(trainingSet)
-
-    val evaluator = MulticlassClassificationEvaluator()
-            .setMetricName("accuracy")
-
-    val accuracy = evaluator.evaluate(predictions)
-    println("Accuracy: $accuracy")
+    decisionTreeClassifier(data)
+    multinomialRegression(data)
+    testMulticassLogisticRegression(data)
 
     spark.stop()
 }
 
+fun multinomialRegression(data: Dataset<Row>) {
+    val (train, test) = split(data)
 
+    val lr = LogisticRegression()
+            .setMaxIter(10)
+            .setRegParam(0.3)
+            .setElasticNetParam(0.8)
+
+    // Fit the model
+    val lrModel = lr.fit(train)
+
+    // Print the coefficients and intercept for multinomial logistic regression
+    println("Coefficients: \n${lrModel.coefficientMatrix()}")
+    println("Intercepts: ${lrModel.interceptVector()}")
+}
+
+fun testMulticassLogisticRegression(data: Dataset<Row>) {
+    val (train, test) = split(data)
+
+    // instantiate the base classifier
+    val classifier = LogisticRegression()
+            .setMaxIter(100)
+            .setTol(1E-6)
+            .setFitIntercept(true)
+
+    // instantiate the One Vs Rest Classifier.
+    val ovr = OneVsRest().setClassifier(classifier)
+
+    // train the multiclass model.
+    val ovrModel = ovr.fit(train)
+
+    // score the model on test data.
+    val predictions = ovrModel.transform(test)
+
+    predictions.show(10, false)
+
+    // obtain evaluator.
+    val evaluator = MulticlassClassificationEvaluator()
+            .setMetricName("accuracy")
+
+    // compute the classification error on test data.
+    val accuracy = evaluator.evaluate(predictions)
+    println("Accuracy = $accuracy")
+}
+
+fun decisionTreeClassifier(data: Dataset<Row>) {
+    val (train, test) = split(data)
+
+    val decisionTreeClassifier = DecisionTreeClassifier()
+            .setLabelCol("label")
+            .setFeaturesCol("features")
+
+    val decisionTreeClassificationModel = decisionTreeClassifier.fit(train)
+
+    val predictions = decisionTreeClassificationModel.transform(test)
+
+    predictions.show(10)
+
+    // obtain evaluator.
+    val evaluator = MulticlassClassificationEvaluator()
+            .setMetricName("accuracy")
+
+    // compute the classification error on test data.
+    val accuracy = evaluator.evaluate(predictions)
+    println("Accuracy = $accuracy")
+}
+
+private fun split(data: Dataset<Row>): Pair<Dataset<Row>, Dataset<Row>> {
+    val split = data.randomSplit(doubleArrayOf(0.8, 0.2))
+    val train = split[0]
+    val test = split[1]
+    return Pair(train, test)
+}
+
+fun loadCovtype(spark: SparkSession): Dataset<Row> {
+    val covtypeData: Dataset<Row> = spark.sqlContext()
+            .read()
+            .option("inferSchema", true)
+            .csv("/Users/bencecserna/Documents/Development/projects/MetaL/resources/covtype/covtype.data.gz")
+            .limit(10000)
+            .toDF()
+            .withColumnRenamed("_c54", "label")
+            .cache()
+
+    return VectorAssembler()
+            .setInputCols(Array(54, { "_c$it" }))
+            .setOutputCol("features")
+            .transform(covtypeData)
+            .select("label", "features")
+
+//    val metadata = MetadataBuilder().putLong("numFeatures", 54).build()
+//
+//    val labelledData = VectorAssembler()
+//            .setInputCols(Array(54, { "_c$it" }))
+//            .setOutputCol("features")
+//            .transform(covtypeData)
+//            .select("label", "features")
+//            .select(Column("label").multiply(1.0).`as`("label"), Column("features").`as`("features", metadata))
+//            .cache()
+
+}
